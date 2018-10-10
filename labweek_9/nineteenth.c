510d48133e477dc60f98b83874e1653e850279ad
Ankesh Bharti <ankeshbharti8@gmail.com>
2018-10-11 00:47:47 +0530

    Added misc code

--- labweek_9/nineteenth.c
+++ labweek_9/nineteenth.c
@@ -15,3 +15,23 @@ int main()
     printf("Reversed Number is\n %d", rev);
     return 0;
 }
+
+/*#include <stdio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+    {
+        // n = n/10
+        n /= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+}
+*/
