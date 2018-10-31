681bb9ddc05c877470d1196552156bc24b891b71
Ankesh Bharti <ankeshbharti8@gmail.com>
2018-10-31 12:29:19 +0530

    correct spoj1 basics

--- spoj/basics/halfo.cpp
+++ spoj/basics/halfo.cpp
@@ -15,31 +15,27 @@ ntc
 
 #include <iostream>
 #include <string.h>
+
 using namespace std;
-void halfofhalf(char a[100][100],int t)
-{
-    for (int i=0; i<t; i++)
-    {
-        cout<<a[i][0];
-        for (int j=1; j<strlen(a[i])/2; j+=2)
-        cout<<a[i][j];
-        cout<<"\n";
 
-    }
-
-}
 int main()
-
 {
-    int t;
-    char array1[100][100];
-    while(t<=100 && t>1)
-    cin>>t;
+int n,k,i=0;
+cin >> n;
+    while(n--)
+    {
+    string s;
+    cin >> s;
+    k=s.length()/2;
 
-    for(int i=0;i<t;i++)
-        cin>>array1[i];
 
-    halfofhalf(array1,t);
-    return 0;
-
+        while(i<k)
+            {
+            cout << s[i];
+            i=i+2;
+            }
+    cout << endl;
+    i=0;
+    }
 }
+
