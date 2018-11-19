5f2e01e3ee5125627f540b5b7713f18ef00907ef
Ankesh Bharti <ankeshbharti8@gmail.com>
2018-11-19 22:25:52 +0530

    Added program to calculate a factorial recursively

---
+++ DecFall/recfactorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+unsigned long long fact(int m);
+
+int main()
+{
+    int num;
+    unsigned long long factorial;
+    scanf("%d", &num);
+    factorial = fact(num);
+    printf("Factorial of %d is %llu", num, factorial);
+    return 0;
+}
+ 
+unsigned long long fact(int num)
+{
+    if(num == 0)
+        return 1;
+    else
+        return num * fact(num - 1);
+}
